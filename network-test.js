475ec35f57678eebabb48649477adff2d989df46
Guillaume Simenel <guillaume.simenel@gmail.com>
2017-12-08 17:24:29 +0800

    file input support
    Added file input support (-f filename)
    Moved API keys config in local.config.js
    Code rework

--- network-test.js
+++ network-test.js
@@ -1,68 +1,138 @@
-// API Google KEY ** Mandatory ** 
-var GoogleApiKey = '';
-// API credentials for Sigfox ** Mandatory ** 
-var sigfoxCredentials = {
-    'Login': "", 
-    'Password': ""
-};
+"use strict;"
+// The script needs a local.config.js file
+// Example :
+/*
+module.exports = {
+    GoogleApiKey : '',
+    sigfoxCredentials : {
+        'Login': "",
+        'Password': ""
+    }
+}
+*/
 
+// INCLUDES
+var api = require('./local.config.js')
 var request = require('request')
 var googleMapsClient = require('@google/maps').createClient({
-  key: GoogleApiKey
+  key: api.GoogleApiKey
 });
+var fs = require('fs');
+var parse = require('csv-parse');
 
-
-if (typeof(GoogleApiKey) == 'undefined' || !GoogleApiKey){
-    console.log("! ERROR : Please edit the geocode.js file and add your Google API Key");
-}
-else {
-    if (typeof(sigfoxCredentials) == 'undefined' || !sigfoxCredentials.Login || !sigfoxCredentials.Password) {
-        console.log("! ERROR : Please edit the geocode.js file and add your Sigfox API Key"); 
-    }
-    else {
-        // Control the number of command line arugments 
-        if (process.argv.length < 3 ){
-            console.log('Usage : node geocode.js "address 1" "address 2" ...');
-            console.log('Returns "address";"Lat";"Lng";"[Array of noise margin for the 3 best Sigfox BS]"');
-            console.log("Example : ");
-            console.log("> node geocode.js '3 rue de Londres 75009 Paris'");
-            console.log("> 3 rue de Londres 75009 Paris France;48.8765789;2.3306177; [ 49, 43, 40 ]");
+function geocodeAndCoverageCheck(ID, address, lat, lng){
+  // ID is simply here to be able to reorder the results after async responses
+  // Function will call Google API to geocode an address, if necessary
+  // And then call the SigfoxCoverage Check.
+  // Geocode an address.
+  if (typeof(lat) == 'undefined' || typeof(lng) == 'undefined'){
+    // If the lat,lng is not provided, first we need to Geocode with Google
+    googleMapsClient.geocode({
+      address: address
+    }, function(err, response) {
+        if (err) {
+          console.dir("ID:" + ID + "**Google API error" + err);
+          return false;
         }
-        else {// print process.argv
-            process.argv.forEach(function (val, index, array) {
-                // Process only the addresses passed
-                if (index > 1){
-                // Geocode an address.
-                googleMapsClient.geocode({
-                  address: val
-                }, function(err, response) {
-                    if (!err) {
-                        // console.log(response.json.results);
-                        var options = {
-                            url: 'http://backend.sigfox.com/api/coverages/global/predictions?lat='+response.json.results[0].geometry.location.lat + '&lng=' + response.json.results[0].geometry.location.lng,
-                            auth: {
-                                user: sigfoxCredentials.Login,
-                                password: sigfoxCredentials.Password
-                            }
-                        };
-                        request(options, function (err, res, body) {
-                            if (err) {
-                                console.dir("**Sigfox API error" + err);
-                            }
-                            else {
-                                //console.dir('headers', res.headers);
-                                //console.dir('status code', res.statusCode);
-                                //console.dir(body);
-                                var SigfoxResult = JSON.parse(body); 
-                                console.log(val + ';' + response.json.results[0].geometry.location.lat + ";" + response.json.results[0].geometry.location.lng + ';', SigfoxResult.margins);
-                            }
-                        })
-                  }else {
-                    console.log("**Google Geocoding Error:" + err);  
-                  }
-                });
-              }
+        else {
+            // console.log(response.json.results);
+            coverageCheck(address, response.json.results[0].geometry.location.lat, response.json.results[0].geometry.location.lng, function(addressCB, latCB, lngCB, coverageResultsCB){
+              console.log(ID + ";" + addressCB + ';' + latCB + ";" + lngCB + ';', coverageResultsCB);
+              return true;
             });
         }
+    });
+  } else {
+    // if lat,lng is provided just do the coverage check with Sigfox API
+    coverageCheck(address, lat, lng, function(addressCB, latCB, lngCB, coverageResultsCB){
+      console.log(ID + ";" + addressCB + ';' + latCB + ";" + lngCB + ';', coverageResultsCB);
+      return true;
+    });
+  }
+}
+
+function coverageCheck(address, lat, lng, callbackFunction){
+  // Sigfox API request
+  // callbackFunction has 4 arguments: address, lat, lng, coverageResults
+  var options = {
+      url: 'http://backend.sigfox.com/api/coverages/global/predictions?lat='+lat + '&lng=' + lng,
+      auth: {
+          user: api.sigfoxCredentials.Login,
+          password: api.sigfoxCredentials.Password
+      }
+  };
+  request(options, function (err, res, body) {
+      if (err) {
+          console.dir("ID:" + ID + "**Sigfox API error" + err);
+          return false;
+      }
+      else {
+          //console.dir('headers', res.headers);
+          //console.dir('status code', res.statusCode);
+          //console.dir(body);
+          callbackFunction(address, lat, lng, JSON.parse(body).margins);
+          return true;
+      }
+  })
+}
+
+/////////////////////////////////
+// MAIN FUNCTION               //
+/////////////////////////////////
+
+if (typeof(api.GoogleApiKey) == 'undefined' || !api.GoogleApiKey){
+  console.log("! ERROR : Please edit the local.conf.js file and add your Google API Key");
+}
+else {
+  if (typeof(api.sigfoxCredentials) == 'undefined' || !api.sigfoxCredentials.Login || !api.sigfoxCredentials.Password) {
+    console.log("! ERROR : Please edit the local.conf.js file and add your Sigfox API Key");
+  }
+  else {
+    // Control the number of command line arugments
+    if (process.argv.length < 3 ){
+      console.log('Usage 1 : node network-test.js "address 1" "address 2" ...');
+      console.log('Returns "ID";"address";"Lat";"Lng";"[Noise margin of the 3 best Sigfox BS]"');
+      console.log("Example : ");
+      console.log("> node network-test.js '3 rue de Londres 75009 Paris'");
+      console.log("> 1;3 rue de Londres 75009 Paris France;48.8765789;2.3306177; [ 49, 43, 40 ]\n");
+
+      console.log('Usage 2 : node network-test.js -f filename.csv');
+      console.log('CSV file should be UTF-8 encoded, and use ; as a separator, it should have a header line with ID, address, lat, lng (example provided)');
+      console.log('The file can provide for each line EITHER address or lat,lng. If both are provided only lat,lng will be used');
+      console.log('Returns "ID";"address";"Lat";"Lng";"[Noise margin of the 3 best Sigfox BS]"\n');
     }
+    else if (process.argv.length == 4 & process.argv[2] == '-f') {
+      // if we receive input '-f filename'
+      var fileName =  process.argv[3];
+      fs.createReadStream(fileName)
+        .pipe(parse({delimiter: ';',
+              columns : ['ID','address','lat','lng'],
+              from : 2
+          }))
+        .on('data', function(csvrow) {
+            //console.log(csvrow);
+            //do something with csvrow
+            if (typeof(csvrow.lat) == 'undefined' || csvrow.lat == ''|| typeof(csvrow.lng) == 'undefined' || csvrow.lng == ''){
+              geocodeAndCoverageCheck(csvrow.ID, csvrow.address);
+            }
+            else {
+              coverageCheck(csvrow.address, csvrow.lat, csvrow.lng, function(addressCB, latCB, lngCB, coverageResultsCB){
+                console.log(csvrow.ID + ";" + addressCB + ';' + latCB + ";" + lngCB + ';', coverageResultsCB);
+              });
+            }
+            // csvData.push(csvrow);
+      })
+      /* .on('end',function() {
+      });*/
+    }
+    else {
+    // if we receive addresses inline
+      process.argv.forEach(function (val, index, array) {
+        // Process only the addresses passed (Arguments 0 and 1 are "node" and the script name)
+        if (index > 1){
+          geocodeAndCoverageCheck(index-1, val);
+        }
+      });
+    }
+  }
 }
--- readme.md
+++ readme.md
@@ -1,29 +1,49 @@
-#Sigfox Network eligiblity test
-This small javascript tool will take addresses, geocode them with Google API, and query Sigfox API in order to test the Sigfox network available over there. 
+# Sigfox Network eligiblity test
+This small javascript tool will take addresses, geocode them with Google API, and query Sigfox API in order to test the Sigfox network available over there.
 
-##Install
+## Install
 
 	git clone https://github.com/sigfox/sigfox-network-test
 	npm install
 
-You must add your own Google API Key and Sigfox API Key !! Go and edit the .js file add at the top in here: 
+You must add your own Google API Key and Sigfox API Key !!
+Add a config.local.js file with :
 
-	// API Google KEY ** Mandatory ** 
-	var GoogleApiKey = '';
-	// API credentials for Sigfox ** Mandatory ** 
-	var sigfoxCredentials = {
-	    'Login': "", 
-	    'Password': ""
-	};
+	module.exports = {
+	    GoogleApiKey : '',
+	    sigfoxCredentials : {
+	        'Login': "",
+	        'Password': ""
+	    }
+	}
 
-##Run
+## Run
 
-	Usage : node network-test.js "address 1" "address 2" ...'
-	Returns "address";"Lat";"Lng";"[Array of noise margin for the 3 best Sigfox BS]"
-	Example :
-	> node network-test.js '3 rue de Londres 75009 Paris'");
-	> 3 rue de Londres 75009 Paris France;48.8765789;2.3306177; [ 49, 43, 40 ]
-	
-##Results
+**Usage 1** : node network-test.js "address 1" "address 2" ...
 
-The Sigfox availability is returned as an array of 3 integers giving the noise margin in db for the three best basestations of the network. If the value is 0, it means that there is less than 3 BS available here. 
+Returns "ID";"address";"Lat";"Lng";"[Noise margin of the 3 best Sigfox BS]"
+
+Example :
+
+	> node network-test.js '3 rue de Londres 75009 Paris'
+	1;3 rue de Londres 75009 Paris France;48.8765789;2.3306177; [ 49, 43, 40 ]
+
+**Usage 2** : node network-test.js -f filename.csv'
+
+CSV file should have a header line with ID, address, lat, lng (example provided)
+
+The file can provide for each line EITHER address or lat,lng. 
+
+If both are provided only lat,lng will be used
+
+Returns "ID";"address";"Lat";"Lng";"[Noise margin of the 3 best Sigfox BS]"
+
+Example : 
+
+	> node network-test.js -f example.csv
+	2;;48.8584442;2.2933859; [ 58, 52, 51 ]
+	1;9 rue de londres 75009 Paris  ;48.87675520000001;2.3299307; [ 49, 43, 40 ]
+
+## Results
+
+The Sigfox availability is returned as an array of 3 integers giving the noise margin in db for the three best basestations of the network. If any value of the array is 0, it means that there is less than 3 BS available here.
